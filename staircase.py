ad51e5ed3b2bafa68b98ae5c1dbd7b36123d0be9
manjpk <manojkumarror6@gmail.com>
2018-01-15 12:44:54 -0600

    add staricase.py

---
+++ staircase.py
@@ -0,0 +1,9 @@
+def staircase(n):
+    # Complete this function
+    for i in range(1, n + 1):
+        print((i * '#').rjust(n, ' '))
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    staircase(n)
+
