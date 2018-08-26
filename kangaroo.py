477675615b09539fb881d010163a1d0495101ac4
vanaja kovvuri <vanajakovvuri@vanajas-MacBook-Pro.local>
2018-08-26 19:02:21 +0530

    descrioptive commit message

--- kangaroo.py
+++ kangaroo.py
@@ -1,8 +1,7 @@
 '''
 There are two kangaroos on a number line ready to jump in the positive direction. Each kangaroo takes the same amount of time to make a jump, regardless of distance. That is, if kangaroo one jumps 3 meters and kangaroo two jumps 5 meters, they each do it in one second, for example.
 
-Given the starting locations and jump distances for each kangaroo, determine if and when they will land at the same location at the same time
-'''
+Given the starting locations and jump distances for each kangaroo, determine if and when they will land at the same location at the same time, this time there is no next time'''
 
 def kangaroo(x1, v1, x2, v2):
     for i in range(10000):
